aa788c193c8bae4c877951c7e9faca76f27962cf
sebasttti <juansjoya_r@outlook.com>
2023-05-09 09:22:34 -0500

    swagger add

--- src/main/java/com/unbosque/edu/co/market/web/config/SwaggerConfig.java
+++ src/main/java/com/unbosque/edu/co/market/web/config/SwaggerConfig.java
@@ -2,6 +2,7 @@ package com.unbosque.edu.co.market.web.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -15,5 +16,11 @@ public class SwaggerConfig {
     public Docket api(){
         return new Docket(DocumentationType.SWAGGER_2).select()
                 .apis(RequestHandlerSelectors.basePackage("com.unbosque.edu.co.market.web.controller")).build();
+
+        /*return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();*/
     }
 }
